4baffe26b1df21c176eac7a9cb2fb2fd90d4bf53
tonykung <tonykung06@gmail.com>
2016-01-02 02:13:58 +0800

    bootstrap db

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 var r = require('rethinkdb');
+var async = require('async');
 
-r.connect(function(err, conn) {
-	if (err) {
-		console.log('connection error', err);
-		return;
-	}
-
-	r.dbCreate('music').run(conn, function(err, res) {
-		if (err) {
-			console.log('failed to create music db', res);
-			return;
-		}
-
-		console.log('response', res);
+var createDb = function(next) {
+	r.connect(function(err, conn) {
+		r.dbCreate('music').run(conn, function(err, res) {
+			conn.close();
+			next(err, res);
+		});
 	});
+};
+
+var createTable = function(name, next) {
+	r.connect({
+		db: 'music'
+	}, function(err, conn) {
+		r.tableCreate(name).run(conn, function(err, res) {
+			conn.close();
+			next(err, res);
+		});
+	});
+};
+
+var createTables = function(next) {
+	async.map(['artists', 'invoices'], createTable, next);
+};
+
+async.series({
+	created: createDb,
+	tables: createTables
+}, function(err, res) {
+	console.log(res);
 });
