abf13951bc359d65dcc644c366e9b62a73a40ace
tonykung <tonykung06@gmail.com>
2016-01-03 13:03:38 +0800

    make import even faster by bulk import

--- importer.js
+++ importer.js
@@ -4,16 +4,12 @@ r.connect({
 	db: 'music'
 }, function(err, conn) {
 	var artists = require('./data/artists');
-	var counter = 1;
 
-	artists.forEach(function(artist) {
-		r.table('artists').insert(artist).run(conn, {
-			durability: 'soft',
-			noreply: true
-		}, function(err, res) {
-			console.log(counter);
-			counter++;
-		});
+	r.table('artists').insert(artists).run(conn, {
+		durability: 'soft',
+		noreply: true
+	}, function(err, res) {
+		console.log(artists.length);
 	});
 
 	conn.close();
