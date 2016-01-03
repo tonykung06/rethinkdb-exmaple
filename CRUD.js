8bef4378dc414365da4ab135959d97f8370d1b3a
tonykung <tonykung06@gmail.com>
2016-01-03 13:55:04 +0800

    mor CRUD

--- CRUD.js
+++ CRUD.js
@@ -48,18 +48,16 @@ r.connect({
 			// 	console.log(res);
 			// });
 
-			console.log('the id', result[0].id);
-
 			r.table('artists').get(result[0].id).run(conn, function(err, res) {
 				console.log(err);
 				console.log(res);
 			});
+
+			conn.close();
 		});
 	});
 
 	// r.table('artists').delete().run(conn, function(err, res) {
 	// 	console.log(res);
 	// });
-
-	conn.close();
 });
