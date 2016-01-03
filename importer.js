a48137ca77449738eb53d475dd8b065be62c98f2
tonykung <tonykung06@gmail.com>
2016-01-03 12:53:02 +0800

    data import

---
+++ importer.js
@@ -0,0 +1,17 @@
+var r = require('rethinkdb');
+
+r.connect({
+	db: 'music'
+}, function(err, conn) {
+	var artists = require('./data/artists');
+	var counter = 1;
+
+	artists.forEach(function(artist) {
+		r.table('artists').insert(artist).run(function(err, res) {
+			console.log(counter);
+			counter++;
+		});
+	});
+
+	conn.close();
+});
