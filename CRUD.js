7bacec0de7ca26a9ba7710de17bb5107821498bc
tonykung <tonykung06@gmail.com>
2016-01-03 13:22:27 +0800

    more CRUD

--- CRUD.js
+++ CRUD.js
@@ -6,6 +6,53 @@ r.connect({
 	r.table('artists').count().run(conn, function(err, res) {
 		console.log(res);
 	});
-	
+
+	r.table('artists').count().typeof().run(conn, function(err, res) {
+		console.log(res);
+	});
+
+	r.table('artists').insert({
+		name: "Another Tony"
+	}).run(conn, function(err, res) {
+		console.log(res);
+	});
+
+	r.table('artists').filter({
+		name: "Another Tony"
+	}).run(conn, function(err, res) {
+		console.log(res);
+	});
+
+	r.table('artists').filter({
+		name: "Another Tony"
+	}).delete().run(conn, function(err, res) {
+		console.log(res);
+	});
+
+	r.table('artists').insert({
+		name: "Another Tony",
+		title: "my title"
+	}).run(conn, function(err, res) {
+		console.log(res);
+	});
+
+	r.table('artists').filter({
+		name: "Another Tony"
+	}).delete().run(conn, function(err, res) {
+		console.log(res);
+
+		r.table('artists').get(res.id).replace({
+			id: res.id,
+			name: 'replaced Tony',
+			email: 'tonykung@tonykung.com'
+		}).run(conn, function(err, res) {
+			console.log(res);
+		});
+	});
+
+	r.table('artists').delete().run(conn, function(err, res) {
+		console.log(res);
+	});
+
 	conn.close();
 });
